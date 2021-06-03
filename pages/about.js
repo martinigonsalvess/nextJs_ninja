f876ebc04e28b0f69d60034e0c364dc055c11a6d
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-06-03 21:15:25 +0200

    initial

---
+++ pages/about.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, error!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+          dignissimos dolorem molestiae beatae corporis corrupti libero illo
+          perspiciatis expedita deleniti.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, error!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Exercitationem minus sequi ullam, corrupti maiores inventore, eaque
+          expedita eligendi voluptatibus hic laudantium perferendis
+          necessitatibus nisi labore enim! Impedit nulla ad repudiandae.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
